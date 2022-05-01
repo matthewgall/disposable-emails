606b8d34667c5ddbe0b70962c6e8f38ce7fbbe62
Matthew Gall <me@matthewgall.dev>
2022-05-01 21:49:28 +0100

    Added guarding to YAML and JSON file building

--- buildJSON.sh
+++ buildJSON.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cat public/list.txt | sed 's/"/\\"/;s/.*/"&",/;1s/^/[/;$s/,$/]/'
+if [ -f public/list.txt ]; then
+    cat public/list.txt | sed 's/"/\\"/;s/.*/"&",/;1s/^/[/;$s/,$/]/'
+else
+    echo "public/list.txt does not exist. Aborting."
+fi
--- buildYAML.sh
+++ buildYAML.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo -e "---\ndomains:"
-cat public/list.txt | sed 's/^/  - \"/g' | sed 's/$/\"/g'
+if [ -f public/list.txt ]; then
+    echo -e "---\ndomains:"
+    cat public/list.txt | sed 's/^/  - \"/g' | sed 's/$/\"/g'
+else
+    echo "public/list.txt does not exist. Aborting."
+fi
