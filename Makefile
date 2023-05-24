9419be74a568e27e48911289db7225282f1c894d
Matthew Gall <me@matthewgall.dev>
2023-05-24 05:57:58 +0100

    Improved sorting, fixed JQ and raw output and cleaned up sources

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ download:
 	@rm -rf public/list.txt public/list.json
 
 	@echo Downloading data to public/list.txt
-	@./build.sh | uniq | sort > public/list.txt
+	@./build.sh | sort | uniq > public/list.txt
 
 	@echo Converting public/list.txt to required formats
 	@./buildJSON.sh > public/list.json
--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-sources=(https://raw.githubusercontent.com/disposable/disposable-email-domains/master/domains.txt
-https://raw.githubusercontent.com/disposable-email-domains/disposable-email-domains/master/disposable_email_blocklist.conf 
-https://raw.githubusercontent.com/MattKetmo/EmailChecker/master/res/throwaway_domains.txt
-https://raw.githubusercontent.com/FGRibreau/mailchecker/master/list.txt
-https://www.stopforumspam.com/downloads/toxic_domains_whole.txt )
+sources=(https://raw.githubusercontent.com/disposable/disposable-email-domains/master/domains.txt https://raw.githubusercontent.com/disposable-email-domains/disposable-email-domains/master/disposable_email_blocklist.conf https://raw.githubusercontent.com/MattKetmo/EmailChecker/master/res/throwaway_domains.txt https://raw.githubusercontent.com/FGRibreau/mailchecker/master/list.txt https://www.stopforumspam.com/downloads/toxic_domains_whole.txt )
 
 for i in "${sources[@]}"
 do
@@ -15,14 +11,13 @@ done
 sources=(https://raw.githubusercontent.com/MISP/misp-warninglists/main/lists/disposable-email/list.json)
 for i in "${sources[@]}"
 do
-  curl -s "$i" | jq '.list[]'
+  curl -s "$i" | jq -r '.list[]'
 done
 
 # Now for more sources that need more processing
-sources=(https://raw.githubusercontent.com/ivolo/disposable-email-domains/master/index.json
-https://raw.githubusercontent.com/ivolo/disposable-email-domains/master/wildcard.json )
+sources=(https://raw.githubusercontent.com/ivolo/disposable-email-domains/master/index.json https://raw.githubusercontent.com/ivolo/disposable-email-domains/master/wildcard.json )
 
 for i in "${sources[@]}"
 do
-  curl -s "$i" | jq '.[]'
+  curl -s "$i" | jq -r '.[]'
 done
