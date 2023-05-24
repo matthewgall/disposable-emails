a0b083ea24624e2ddbfc89f8aaca8e1820ae40f4
Matthew Gall <matthew@noreply.matthewgall.codes>
2023-05-24 04:48:16 +0000

    Added stopforumspam.com source

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 sources=(https://raw.githubusercontent.com/disposable/disposable-email-domains/master/domains.txt
 https://raw.githubusercontent.com/disposable-email-domains/disposable-email-domains/master/disposable_email_blocklist.conf 
 https://raw.githubusercontent.com/MattKetmo/EmailChecker/master/res/throwaway_domains.txt
-https://raw.githubusercontent.com/FGRibreau/mailchecker/master/list.txt )
+https://raw.githubusercontent.com/FGRibreau/mailchecker/master/list.txt
+https://www.stopforumspam.com/downloads/toxic_domains_whole.txt )
 
 for i in "${sources[@]}"
 do
