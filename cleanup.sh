4699df60ab121e1d5f1f3d23eae7c729a5443522
Matthew Gall <me@matthewgall.dev>
2023-05-25 22:39:19 +0100

    Added improved external allowlist support

--- cleanup.sh
+++ cleanup.sh
@@ -6,6 +6,17 @@ grep -Fvxf /tmp/tranco.txt public/list.txt > /tmp/list.txt
 mv /tmp/list.txt public/list.txt
 rm /tmp/tranco.txt
 
+# Now, we process some allowlists
+allowlists=(https://raw.githubusercontent.com/disposable/disposable/master/blacklist.txt)
+for i in "${allowlists[@]}"
+do
+    id=$(date +%s | sha256sum | base64 | head -c 12)
+    curl -s "$i" -o /tmp/$id.txt
+    grep -Fvxf /tmp/$id.txt public/list.txt > /tmp/list.txt
+    mv /tmp/list.txt public/list.txt
+    rm /tmp/$id.txt
+done
+
 # And remove manually blocked domains
 while IFS="" read -r p || [ -n "$p" ]
 do
