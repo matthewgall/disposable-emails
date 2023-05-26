3fd476c884a936b4086c9d4e88e6ac69c4e14f12
Matthew Gall <me@matthewgall.dev>
2023-05-26 23:00:12 +0100

    Added improved error handling, using the validator library

--- functions/api/[email].js
+++ functions/api/[email].js
@@ -1,19 +1,23 @@
+import isEmail from 'validator/lib/isEmail';
 import Data from '../../public/list.json';
 
 export async function handle({ request, env }) {
     let resp = {
-        'success': true,
-        'isDisposable': false
+        'success': true
     }
+
     let email = new URL(request.url).pathname.replace('/api/', '');
+    if (!isEmail(email)) {
+        resp.success = false;
+        resp.message = `It does not appear that you provided a valid e-mail address to check`
+        return new Response(JSON.stringify(resp), {headers: {'Content-Type': 'application/json'}});
+    }
 
     // Next, we get the domain, by splitting the e-mail
     let domain = email.split('@')[1];
-
-    // Now, we see if the data is in our array
+    // Now, we see if the data is in our array and confirming if the domain is a throwaway
     if (Data.includes(domain)) {
         resp.isDisposable = true;
-    
     }
     return new Response(JSON.stringify(resp), {headers: {'Content-Type': 'application/json'}});
 }
