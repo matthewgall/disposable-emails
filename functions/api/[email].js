3e84e8b4f0fdac37a3a9dc4f0b9e7ac8381d311b
Matthew Gall <me@matthewgall.dev>
2023-05-28 17:43:43 +0100

    Added improved status returns via HTTP and added CORS

--- functions/api/[email].js
+++ functions/api/[email].js
@@ -2,6 +2,13 @@ import isEmail from 'validator/lib/isEmail';
 import isFQDN from 'validator/lib/isFQDN';
 import Data from '../../public/list.json';
 
+let headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, cf-access-client-id, cf-access-client-secret'
+}
+
 export async function handle({ request, env }) {
     let resp = {
         'success': true
@@ -11,7 +18,7 @@ export async function handle({ request, env }) {
     if (!isEmail(email)) {
         resp.success = false;
         resp.message = `It does not appear that you provided a valid e-mail address to check`
-        return new Response(JSON.stringify(resp), {headers: {'Content-Type': 'application/json'}});
+        return new Response(JSON.stringify(resp), {status, 400, headers: headers});
     }
 
     // Next, we get the domain, by splitting the e-mail
@@ -19,15 +26,18 @@ export async function handle({ request, env }) {
     if (!isFQDN(domain)) {
         resp.success = false;
         resp.message = `It does not appear that you provided a valid e-mail address to check`
-        return new Response(JSON.stringify(resp), {headers: {'Content-Type': 'application/json'}});
+        return new Response(JSON.stringify(resp), {status: 400, headers: headers});
     }
 
     // Now, we see if the data is in our array and confirming if the domain is a throwaway
     resp.isDisposable = false;
+    let status = 404;
     if (Data.includes(domain)) {
         resp.isDisposable = true;
+        status = 200;
     }
-    return new Response(JSON.stringify(resp), {headers: {'Content-Type': 'application/json'}});
+
+    return new Response(JSON.stringify(resp), {status: status, headers: headers});
 }
 
 export async function onRequest({ request, env }) {
