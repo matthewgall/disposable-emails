9f093da14ce0d5840af53321eb31292a1e0f8df5
Matthew Gall <me@matthewgall.dev>
2023-07-08 08:04:21 +0100

    More logic improvements for domain lookup support

--- functions/api/v1/[email].js
+++ functions/api/v1/[email].js
@@ -15,7 +15,7 @@ export async function handle({ request, env }) {
     }
 
     let email = new URL(request.url).pathname.replace('/api/v1', '').toLowerCase().trim();
-    if (isEmail(email) === false && isFQDN(email) === false) {
+    if (!isEmail(email) && !isFQDN(email)) {
         resp.success = false;
         resp.message = `It does not appear that you provided a valid e-mail address or domain name to check`
         return new Response(JSON.stringify(resp), {status: 400, headers: headers});
@@ -23,15 +23,16 @@ export async function handle({ request, env }) {
 
     // Next, we get the domain, by splitting the e-mail (if it is one)
     let domain = email;
-    if (isEmail(email)) domain = email.split('@')[1];
+    if (isEmail(email)) {
+        domain = email.split('@')[1];
 
-    // Now validate the domain is valid
-    if (!isFQDN(domain)) {
-        resp.success = false;
-        resp.message = `It does not appear that you provided a valid e-mail address or domain name to check`
-        return new Response(JSON.stringify(resp), {status: 400, headers: headers});
+        if (!isFQDN(domain)) {
+            resp.success = false;
+            resp.message = `It does not appear that you provided a valid e-mail address or domain name to check`
+            return new Response(JSON.stringify(resp), {status: 400, headers: headers});
+        }
     }
-
+    
     // Now, we see if the data is in our array and confirming if the domain is a throwaway
     resp.isDisposable = false;
     if (Data.includes(domain)) {
