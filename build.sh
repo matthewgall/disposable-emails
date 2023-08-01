fc5cb7975abd620bc2d09eb277e51946ab428a14
Matthew Gall <me@matthewgall.dev>
2023-08-01 12:57:35 +0100

    Updated paths to JQ in build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ done
 sources=(https://raw.githubusercontent.com/MISP/misp-warninglists/main/lists/disposable-email/list.json)
 for i in "${sources[@]}"
 do
-  curl -s "$i" | $jq -r '.list[]'
+  curl -s "$i" | ./jq -r '.list[]'
 done
 
 # Now for more sources that need more processing
@@ -21,7 +21,7 @@ sources=(https://raw.githubusercontent.com/ivolo/disposable-email-domains/master
 
 for i in "${sources[@]}"
 do
-  curl -s "$i" | $jq -r '.[]'
+  curl -s "$i" | ./jq -r '.[]'
 done
 
 # And finally the manual blocklist
